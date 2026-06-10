afd7c962418d8a21ae45e23208659ca808fa27c4
n3tw0rth <40062006+n3tw0rth@users.noreply.github.com>
2026-06-10 20:35:10 +0530

    fix: rust builds

--- modules/langs/rust/rust.nix
+++ modules/langs/rust/rust.nix
@@ -2,10 +2,16 @@
 
 {
   home.packages = with pkgs; [
-rustc
-  cargo
-  clippy
-  rustfmt
-  rust-analyzer
+    rustc
+    cargo
+    clippy
+    rustfmt
+    rust-analyzer
+    pkg-config
+    openssl.dev
   ];
+
+  home.sessionVariables = {
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  };
 }
